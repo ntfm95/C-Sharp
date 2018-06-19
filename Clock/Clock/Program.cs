449e11f32fcff10436f18740d4989dfc859c0043
Nathan Ferreira da Silva <ntfm95@gmail.com>
2018-06-19 17:17:37 -0300

    Stopwatch

---
+++ Clock/Clock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+
+            Console.WriteLine("1 for start.\n2 for stop.\n3 close the project.");
+            stopWatch.Butt = Console.ReadLine();
+
+            while(stopWatch.Butt != "3")
+            {
+                if (stopWatch.Butt == "1")
+                    stopWatch.Start();
+                else if (stopWatch.Butt == "2")
+                {
+                    stopWatch.Stop();
+                }
+                Console.WriteLine("1 for start.\n2 for stop.\n3 close the project");
+                stopWatch.Butt = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Clock/Clock/Stopwatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Clock
+{
+    class Stopwatch
+    {
+
+        private bool _isRunning = false;
+        private DateTime _nowTime;
+        public string Butt { get; set; }
+
+
+        public void Start()
+        {
+            if (_isRunning)
+                throw new System.ArgumentException("You cannot initialize twice");
+
+            else
+            {
+
+                _nowTime = DateTime.Now;
+                _isRunning = true;
+            }
+
+        }
+        public void Stop()
+        {
+            if (!_isRunning)
+                throw new System.ArgumentException("Stopwatch is doesn't running");
+            else
+            {
+                var timeSpan = DateTime.Now - _nowTime;
+                Console.WriteLine($"The Count : {timeSpan.Hours}:{timeSpan.Minutes}:{timeSpan.Seconds}:{timeSpan.Milliseconds}");
+
+                _isRunning = false;
+            }
+            
+        }
+    }
+
+}
